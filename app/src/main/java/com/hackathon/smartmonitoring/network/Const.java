4a4e4f37f95bca74d9881978eeb1f5b102002192
VladisssLuv <vladissslav19@gmail.com>
2023-11-04 20:00:27 +0300

    update: HEAD_URL

--- app/src/main/java/com/hackathon/smartmonitoring/network/Const.java
+++ app/src/main/java/com/hackathon/smartmonitoring/network/Const.java
@@ -4,7 +4,7 @@ import com.hackathon.smartmonitoring.util.SharedPref;
 
 public class Const {
 
-    public static final String HEAD_URL = "https://sm.kaboom.pro/api/";
+    public static final String HEAD_URL = "https://newestsm.kaboom.pro/api/";
     public static final String LOGS = "log";
     public static final String KILL_DB = "log/"+ SharedPref.getKillId();
     public static final String WEB_SOCKET ="https://newestsm.kaboom.pro/jubs/logs";
